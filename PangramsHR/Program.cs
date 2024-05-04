d5fe9f028193f4490a9f4973ccc62577a6a0e2d5
Deyan Diulgerov <chukstv@abv.bg>
2024-05-04 17:34:34 +0300

    Add files via upload

---
+++ PangramsHR/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PangramsHR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PangramsHR("The quick brown fox jumps over the lazy dog"));
+            Console.WriteLine(PangramsHR("We promptly judged antique ivory buckles for the prize"));
+        }
+        public static string PangramsHR(string s)
+        {
+            var map = new Dictionary<char, int>();
+            foreach (char letter in s)
+            {
+                if (!map.ContainsKey(letter))
+                    map.Add(letter, 1);
+                else
+                    map[letter]++;
+            }
+            if (map.Count == 26)
+                return "pangram";
+            return "not pangram";
+        }
+    }
+}
