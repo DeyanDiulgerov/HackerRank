90eb5cf846fde740759db77a6db79aa6fbcab363
Deyan Diulgerov <chukstv@abv.bg>
2024-05-05 13:40:16 +0300

    Add files via upload

---
+++ MarsExplorationHR/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsExplorationHR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MarsExplorationHR("SOSSPSSQSSOR"));
+            Console.WriteLine(MarsExplorationHR("SOSSOT"));
+            Console.WriteLine(MarsExplorationHR("SOSSOSSOS"));
+        }
+        public static int MarsExplorationHR(string s)
+        {
+            int resCount = 0;
+            for (int i = 0; i < s.Length; i += 3)
+            {
+                if (s[i] != 'S')
+                    resCount++;
+                if (s[i + 1] != 'O')
+                    resCount++;
+                if (s[i + 2] != 'S')
+                    resCount++;
+            }
+            return resCount;
+        }
+    }
+}
