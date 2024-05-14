2c8231391299da4d7bfcb98ac5d01dc029ed40ad
Deyan Diulgerov <chukstv@abv.bg>
2024-05-14 19:32:02 +0300

    Add files via upload

---
+++ CatsAndAMouseHR/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatsAndAMouseHR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CatsAndAMouseHR(2, 5, 4));
+            Console.WriteLine(CatsAndAMouseHR(1, 2, 3));
+            Console.WriteLine(CatsAndAMouseHR(1, 3, 2));
+        }
+        static string CatsAndAMouseHR(int x, int y, int z)
+        {
+            //x == a, y == b, z == mouse
+            if (Math.Abs(z - y) < Math.Abs(z - x))
+                return "Cat B";
+            else if (Math.Abs(z - y) > Math.Abs(z - x))
+                return "Cat A";
+            else
+                return "Mouse C";
+        }
+    }
+}
