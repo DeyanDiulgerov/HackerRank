dcb0fba2b4f05f06e3ddbf323b9af4544dcd0d5a
Deyan Diulgerov <chukstv@abv.bg>
2024-05-15 18:17:32 +0300

    Add files via upload

---
+++ UtopianTreeHR/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtopianTreeHR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(UtopianTreeHR(5));//14
+            Console.WriteLine(UtopianTreeHR(0));//1
+            Console.WriteLine(UtopianTreeHR(1));//2
+            Console.WriteLine(UtopianTreeHR(4));//7
+        }
+        public static int UtopianTreeHR(int n)
+        {
+            //double -> +1 -> repeat...
+            if (n == 0)
+                return 1;
+            int counter = 1;
+            int result = 1;
+            while (counter <= n)
+            {
+                if (counter % 2 != 0)
+                    result *= 2;
+                else
+                    result++;
+
+                counter++;
+            }
+            return result;
+        }
+    }
+}
