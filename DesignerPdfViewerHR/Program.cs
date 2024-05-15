f3a05179885b09a82c3d32a456c8a86dc612e9dc
Deyan Diulgerov <chukstv@abv.bg>
2024-05-15 17:28:57 +0300

    Add files via upload

---
+++ DesignerPdfViewerHR/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignerPdfViewerHR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DesignerPdfViewerHR(
+                new List<int>() { 1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, },
+                "abc"));
+            //9
+            Console.WriteLine(DesignerPdfViewerHR(
+               new List<int>() { 1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 7, },
+               "zaba"));
+            //28
+        }
+        public static int DesignerPdfViewerHR(List<int> h, string word)
+        {
+            var map = new Dictionary<char, int>();
+            for (int i = 0; i < h.Count; i++)
+                map.Add((char)(i + 97), h[i]);
+
+            int max = 0;
+            foreach (char letter in word)
+                max = Math.Max(max, map[letter]);
+            return max * word.Length;
+        }
+    }
+}
