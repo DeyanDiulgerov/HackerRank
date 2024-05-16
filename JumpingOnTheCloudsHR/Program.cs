b70ff936a79c06532c3278631c1deefa6b1314f3
Deyan Diulgerov <chukstv@abv.bg>
2024-05-16 15:35:01 +0300

    Add files via upload

---
+++ JumpingOnTheCloudsHR/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JumpingOnTheCloudsHR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(JumpingOnTheCloudsHR(new List<int>() { 0, 1, 0, 0, 0, 1, 0 }));
+            Console.WriteLine(JumpingOnTheCloudsHR(new List<int>() { 0, 0, 1, 0, 0, 1, 0 }));
+        }
+        public static int JumpingOnTheCloudsHR(List<int> c)
+        {
+            int count = 0;
+            for (int i = 0; i < c.Count - 1; i += 0)
+            {
+                if (i < c.Count - 2 && c[i + 2] == 0)
+                    i += 2;
+                else
+                    i++;
+                count++;
+            }
+            return count;
+        }
+    }
+}
